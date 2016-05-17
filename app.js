06ca953137f7ea3b621d1e20615e1dd8b00e19f5
rayk216 <raykoren@gmail.com>
2016-05-17 14:25:28 -0600

    pads all working, sound banks working, ready to start work on api

--- app.js
+++ app.js
@@ -1,89 +1,99 @@
-
-  $(document).ready(function() {
-///// AudioContext init /////
+$(document).ready(function() {
+    ///// AudioContext init /////
     var context;
     window.addEventListener('load', init, false);
+
     function init() {
-      try {
-        // Fix up for prefixing
-        window.AudioContext = window.AudioContext||window.webkitAudioContext;
-        context = new AudioContext();
-      }
-      catch(e) {
-        alert('Web Audio API is not supported in this browser');
-      }
+        try {
+            // Fix up for prefixing
+            window.AudioContext = window.AudioContext || window.webkitAudioContext;
+            context = new AudioContext();
+        } catch (e) {
+            alert('Web Audio API is not supported in this browser');
+        }
     }
 
-///////// osc for pad 1 //////
-    // var audioCtx = new AudioContext();
-    //     var osc = document.getElementById('touchPad');
-    //
-    //   osc.onmousedown = function(){
-    //     var oscPitch = document.getElementById('oscPitch').value;
-    //     //var oscGain = document.getElementById('oscGain').value;
-    //
-    //     oscillator = audioCtx.createOscillator();
-    //     gainNode = audioCtx.createGain();
-    //
-    //       oscillator.type = "sine";
-    //       oscillator.frequency.value = oscPitch;
-    //       //gainNode.gain.value = oscGain;
-    //       //oscillator.connect(audioCtx.destination); // not needed b/c gain node
-    //       oscillator.start(0);
-    //
-    //       oscillator.connect(gainNode);
-    //       gainNode.connect(audioCtx.destination);
-    //
-    //   };
-    //
-    // osc.onmouseup = function(){
-    //   oscillator.disconnect();
-    // }
-///////// end osc for pad 1 //////
-//////// Load sounds into Buffer ////
+
+    //////// Load sounds into Buffer ////
     window.onload = init;
     var context;
     var bufferLoader;
+    var sel;
+    $("form").submit(function(event) {
+        event.preventDefault();
+        sel = $(".soundBank").val();
+        context.close();
+          $(".touchPad").off();
+          $(document).off('keydown');
+          init();
+
+        console.log(sel);
+    });
 
     function init() {
-      // Fix up prefixing
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      context = new AudioContext();
+        ////// Choose Sound Bank ////
+        if ($(".soundBank").val() === 'tone1') {
+            window.AudioContext = window.AudioContext || window.webkitAudioContext;
+            context = new AudioContext();
 
-      bufferLoader = new BufferLoader(
-        context,
-        [
-          '../samples/10.wav',
-          '../samples/11.wav',
-          '../samples/12.wav',
-          '../samples/13.wav',
-          '../samples/14.wav',
-          '../samples/15.wav',
-          '../samples/16.wav',
-          '../samples/10.wav',
-          '../samples/11.wav',
-          '../samples/12.wav',
-          '../samples/13.wav',
-          '../samples/14.wav',
-          '../samples/15.wav',
-          '../samples/16.wav',
-          '../samples/15.wav',
-          '../samples/16.wav',
-        ],
-        finishedLoading
-        );
-      bufferLoader.load();
+            bufferLoader = new BufferLoader(
+                context, [
+                    '../samples/Bank 1/10.wav',
+                    '../samples/Bank 1/11.wav',
+                    '../samples/Bank 1/12.wav',
+                    '../samples/Bank 1/13.wav',
+                    '../samples/Bank 1/14.wav',
+                    '../samples/Bank 1/15.wav',
+                    '../samples/Bank 1/16.wav',
+                    '../samples/Bank 1/10.wav',
+                    '../samples/Bank 1/11.wav',
+                    '../samples/Bank 1/12.wav',
+                    '../samples/Bank 1/13.wav',
+                    '../samples/Bank 1/14.wav',
+                    '../samples/Bank 1/15.wav',
+                    '../samples/Bank 1/16.wav',
+                    '../samples/Bank 1/15.wav',
+                    '../samples/Bank 1/16.wav',
+                ],
+                finishedLoading
+            );
+        } else {
+            window.AudioContext = window.AudioContext || window.webkitAudioContext;
+            context = new AudioContext();
+            bufferLoader = new BufferLoader(
+                context, [
+                    '../samples/Bank 2/Koto 1.wav',
+                    '../samples/Bank 2/Koto 2.wav',
+                    '../samples/Bank 2/Koto 3.wav',
+                    '../samples/Bank 2/Koto 4.wav',
+                    '../samples/Bank 2/Koto 5.wav',
+                    '../samples/Bank 2/Koto 6.wav',
+                    '../samples/Bank 2/Koto 7.wav',
+                    '../samples/Bank 2/Koto 1.wav',
+                    '../samples/Bank 2/Koto 2.wav',
+                    '../samples/Bank 2/Koto 3.wav',
+                    '../samples/Bank 2/Koto 4.wav',
+                    '../samples/Bank 2/Koto 5.wav',
+                    '../samples/Bank 2/Koto 6.wav',
+                    '../samples/Bank 2/Koto 7.wav',
+                    '../samples/Bank 2/Koto 1.wav',
+                    '../samples/Bank 2/Koto 2.wav',
+                ],
+                finishedLoading
+            );
+        }
+        bufferLoader.load();
     }
-///// playSound function declaration /////
+    ///// playSound function declaration /////
     function playSound(buffer) {
-      var source = context.createBufferSource();
-      source.buffer = buffer;
-      source.connect(context.destination);
-      source.start(0);
+        var source = context.createBufferSource();
+        source.buffer = buffer;
+        source.connect(context.destination);
+        source.start(0);
     }
 
-////// multi sound func //////
-      function finishedLoading(bufferList) {
+    ////// multi sound func //////
+    function finishedLoading(bufferList) {
         var t1 = context.createBufferSource();
         var t2 = context.createBufferSource();
         var t3 = context.createBufferSource();
@@ -136,142 +146,147 @@
         t16.connect(context.destination);
 
         $(".touchPad").click(function() {
-        var str = $(this).attr("class");
-        var padID = str.replace("col-md-3 touchPad ", "")
-        var note = padID + ".buffer";
-        console.log(note);
+            var str = $(this).attr("class");
+            var padID = str.replace("col-xs-3 touchPad ", "")
+            var note = padID + ".buffer";
+            //console.log(note);
 
-        switch (note) {
-          case "t1.buffer":
-          note = bufferList[0];
-            break;
-          case "t2.buffer":
-          note = bufferList[1];
-            break;
-          case "t3.buffer":
-          note = bufferList[2];
-            break;
-          case "t4.buffer":
-          note = bufferList[3];
-            break;
-          case "t5.buffer":
-          note = bufferList[4];
-            break;
-          case "t6.buffer":
-          note = bufferList[5];
-            break;
-          case "t7.buffer":
-          note = bufferList[6];
-            break;
-          case "t8.buffer":
-          note = bufferList[7];
-            break;
-          case "t9.buffer":
-          note = bufferList[8];
-            break;
-          case "t10.buffer":
-          note = bufferList[9];
-            break;
-          case "t11.buffer":
-          note = bufferList[10];
-            break;
-          case "t12.buffer":
-          note = bufferList[11];
-            break;
-          case "t13.buffer":
-          note = bufferList[12];
-            break;
-          case "t14.buffer":
-          note = bufferList[13];
-            break;
-          case "t15.buffer":
-          note = bufferList[14];
-            break;
-          case "t16.buffer":
-          note = bufferList[15];
-            break;
-        }
-          playSound(note);
+            switch (note) {
+                case "t1.buffer":
+                    note = bufferList[0];
+                    break;
+                case "t2.buffer":
+                    note = bufferList[1];
+                    break;
+                case "t3.buffer":
+                    note = bufferList[2];
+                    break;
+                case "t4.buffer":
+                    note = bufferList[3];
+                    break;
+                case "t5.buffer":
+                    note = bufferList[4];
+                    break;
+                case "t6.buffer":
+                    note = bufferList[5];
+                    break;
+                case "t7.buffer":
+                    note = bufferList[6];
+                    break;
+                case "t8.buffer":
+                    note = bufferList[7];
+                    break;
+                case "t9.buffer":
+                    note = bufferList[8];
+                    break;
+                case "t10.buffer":
+                    note = bufferList[9];
+                    break;
+                case "t11.buffer":
+                    note = bufferList[10];
+                    break;
+                case "t12.buffer":
+                    note = bufferList[11];
+                    break;
+                case "t13.buffer":
+                    note = bufferList[12];
+                    break;
+                case "t14.buffer":
+                    note = bufferList[13];
+                    break;
+                case "t15.buffer":
+                    note = bufferList[14];
+                    break;
+                case "t16.buffer":
+                    note = bufferList[15];
+                    break;
+            }
+            playSound(note);
         });
-        window.addEventListener("keydown", function(event){
-          console.log(event.keyCode);
-          var key = event.keyCode;
-          switch (key) {
-            case "81":
-            key = bufferList[0];
-                break;
-            // case "87":
-            // note = bufferList[1];
-            //   break;
-            // case "69":
-            // note = bufferList[2];
-            //   break;
-            // case "82":
-            // note = bufferList[3];
-            //   break;
-            // case "t5.buffer":
-            // note = bufferList[4];
-            //   break;
-            // case "t6.buffer":
-            // note = bufferList[5];
-            //   break;
-            // case "t7.buffer":
-            // note = bufferList[6];
-            //   break;
-            // case "t8.buffer":
-            // note = bufferList[7];
-            //   break;
-            // case "t9.buffer":
-            // note = bufferList[8];
-            //   break;
-            // case "t10.buffer":
-            // note = bufferList[9];
-            //   break;
-            // case "t11.buffer":
-            // note = bufferList[10];
-            //   break;
-            // case "t12.buffer":
-            // note = bufferList[11];
-            //   break;
-            // case "t13.buffer":
-            // note = bufferList[12];
-            //   break;
-            // case "t14.buffer":
-            // note = bufferList[13];
-            //   break;
-            // case "t15.buffer":
-            // note = bufferList[14];
-            //   break;
-            // case "t16.buffer":
-            // note = bufferList[15];
-            //   break;
-          }
-          playSound(key);
+
+        $(document).keydown(function(event) {
+            console.log(event.keyCode);
+            var note = event.keyCode;
+            switch (note) {
+                case 81:
+                    note = bufferList[0];
+                    break;
+                case 87:
+                    note = bufferList[1];
+                    break;
+                case 69:
+                    note = bufferList[2];
+                    break;
+                case 82:
+                    note = bufferList[3];
+                    break;
+                case 89:
+                    note = bufferList[4];
+                    break;
+                case 85:
+                    note = bufferList[5];
+                    break;
+                case 73:
+                    note = bufferList[6];
+                    break;
+                case 79:
+                    note = bufferList[7];
+                    break;
+                case 65:
+                    note = bufferList[8];
+                    break;
+                case 83:
+                    note = bufferList[9];
+                    break;
+                case 68:
+                    note = bufferList[10];
+                    break;
+                case 70:
+                    note = bufferList[11];
+                    break;
+                case 72:
+                    note = bufferList[12];
+                    break;
+                case 74:
+                    note = bufferList[13];
+                    break;
+                case 75:
+                    note = bufferList[14];
+                    break;
+                case 76:
+                    note = bufferList[15];
+                    break;
+            }
+            playSound(note);
         });
-        // create and send request (for reverb audio)
+    };
 
 
 
-        };
-        // var request = new XMLHttpRequest(),
-        //   url = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/11314/AbernyteGrainSilo.m4a";
-        //
-        // request.open("GET", url, true);
-        // request.responseType = "arraybuffer";
-        // request.addEventListener("load", onLoad);
-        //
-        // request.send();
 
-        // $(".touchPad").click(function() {
-        // console.log($(this).attr("class"));
-        // //playSound(source7.buffer);
-        // });
 
-        // var someVar;
-        // someVar = some_other_function();
-        // alert(someVar);
-        // div.addEventListener("click", function(){
-        //     some_function(someVar);
-        // }, false);
 
-  });
+    // create and send request (for reverb audio)
+
+    // var request = new XMLHttpRequest(),
+    //   url = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/11314/AbernyteGrainSilo.m4a";
+    //
+    // request.open("GET", url, true);
+    // request.responseType = "arraybuffer";
+    // request.addEventListener("load", onLoad);
+    //
+    // request.send();
+
+    // $(".touchPad").click(function() {
+    // console.log($(this).attr("class"));
+    // //playSound(source7.buffer);
+    // });
+
+    // var someVar;
+    // someVar = some_other_function();
+    // alert(someVar);
+    // div.addEventListener("click", function(){
+    //     some_function(someVar);
+    // }, false);
+
+});
